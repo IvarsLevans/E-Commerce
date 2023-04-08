aae3a697d8baf804e303d3b9090d0e9b1bc1d660
ivarslevans <ivars.levans@gmail.com>
2023-04-08 21:48:51 +0300

    Improved data

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const Product = require("../models/Product");
+const { verifyTokenAndAdmin } = require("./verifyToken");
 
 //CREATE
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
